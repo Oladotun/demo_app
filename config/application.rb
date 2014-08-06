d2d84c36ec1b25be8e2f88629d91f1240507576a
Oladotun Opasina <oladotunopasina@yahoo.com>
2014-08-06 08:29:57 -0400

    Fixed precompile issues for Heroku

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,9 @@ module DemoApp
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    # Heroku precompile
+    config.assets.initialize_on_precompile = false
+
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
